e43bd4878fa304d669adf89691308c3c0587c562
ayoub <ayoublamhidra@gmail.com>
2014-06-05 15:20:46 +0100

    05/06/2014 15:19

---
+++ PFE.Web/App/services/validation.js
@@ -0,0 +1,25 @@
+﻿define(function () {
+
+
+    //validation
+    var mustEqual = function (val, other) {
+        return val == other();
+    };
+
+    var dateValidation = function (val, other) {
+        return val >= other();
+    };
+
+    var digitValidation = function (val, max) {
+        return val <= max;
+    }
+
+    var validation = {
+        mustEqual: mustEqual,
+        dateValidation: dateValidation,
+        digitValidation: digitValidation
+    };
+
+    return validation;
+
+});
